c941ba619e4dd01f378017764e09ecf52d0af927
jian0732 <z0653400@gmail.com>
2022-10-19 09:00:45 +0800

    1019

---
+++ prj認真版嗎/wwwroot/js/JS.js
@@ -0,0 +1,12 @@
+﻿
+showmodal = (url, title) => {
+    $.ajax({
+        type: 'get',
+        url:url,
+        success: function (res) {
+            $("#newstaticBackdrop . modal-body").html(res);
+            $("#newstaticBackdrop . modal-title").html(title);
+            $("#newstaticBackdrop").modal('show');
+        }
+    })
+}
