28ebfbd321c454c73a60415d400ea7d6dddf4253
jian0732 <z0653400@gmail.com>
2022-10-25 11:27:20 +0800

    10256666

---
+++ prj認真版嗎/ViewComponents/Commentdata.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using prjMvcCoreModel.ViewModel;
+using prj認真版嗎.Models;
+using prj認真版嗎.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace prj認真版嗎.ViewComponents
+{
+    [Microsoft.AspNetCore.Mvc.ViewComponent]
+    public class Commentdata:Microsoft.AspNetCore.Mvc.ViewComponent
+    {
+
+        public async Task<IViewComponentResult> InvokeAsync(C評論畫面 x)
+        {
+            return View(x);  
+        }
+    }
+}
--- prj認真版嗎/ViewModel/C評論畫面.cs
+++ prj認真版嗎/ViewModel/C評論畫面.cs
@@ -1,6 +1,7 @@
 ﻿using prj認真版嗎.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -14,5 +15,49 @@ namespace prj認真版嗎.ViewModel
         }
         private Comment _com;
 
+        public Comment comment
+        {
+            get { return _com; }
+            set { _com = value; }
+        }
+        [DisplayName("評價編號")]
+        public int CommentId
+        {
+            get { return _com.CommentId; }
+            set { _com.CommentId = value; }
+        }
+        [DisplayName("會員編號")]
+        public int MembersId
+        {
+            get { return _com.MembersId; }
+            set { _com.MembersId = value; }
+        }
+        [DisplayName("產品編號")]
+        public int TravelProductId
+        {
+            get { return _com.TravelProductId; }
+            set { _com.TravelProductId = value; }
+        }
+        [DisplayName("評價內容")]
+        public string CommentText
+        {
+            get { return _com.CommentText; }
+            set { _com.CommentText = value; }
+        }
+        [DisplayName("星數")]
+        public int Star
+        {
+            get { return _com.Star; }
+            set { _com.Star = value; }
+        }
+        [DisplayName("評價時間")]
+        public string CommentDate
+        {
+            get { return _com.CommentDate; }
+            set { _com.CommentDate = value; }
+        }
+         
+        public string 產品名稱 { get; set; }
+        public string 會員名稱 { get; set; }
     }
 }
